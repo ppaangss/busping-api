61609736415f383b5ed9242d3b772ad391d86174
ppaangss <ppaangss@naver.com>
2026-02-02 15:25:51 +0900

    chore: ci test 코드 실패 코드 주석 처리

--- src/test/java/com/station/alarm/test/SampleFailTest.java
+++ src/test/java/com/station/alarm/test/SampleFailTest.java
@@ -6,10 +6,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SampleFailTest {
 
-    @Test
-    void 덧셈_오류테스트() {
-        int result = 1 + 2;
-
-        assertThat(result).isEqualTo(4);
-    }
+    // ci 에서 테스트 오류 확인용 코드
+    // 주석 처리
+//    @Test
+//    void 덧셈_오류테스트() {
+//        int result = 1 + 2;
+//
+//        assertThat(result).isEqualTo(4);
+//    }
 }
