ae3d4269e03037feee107264522984ce5ec2f66a
ppaangss <ppaangss@naver.com>
2026-02-02 14:24:04 +0900

    초기 설정

---
+++ src/test/java/com/station/alarm/StationAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.station.alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
